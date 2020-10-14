bf3534de8da48071a374a72b932927dcb457c99e
jeffreyo3 <jeffrey.orndorff@gmail.com>
2020-10-13 21:35:27 -0700

    Complete 'Login' GUI to learn how to setup GUI. TODO: setup TicTacToe game

--- src/GUI.java
+++ src/GUI.java
@@ -1,6 +1,16 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GUI {
+public class GUI implements ActionListener {
+
+    private static JLabel userLabel;
+    private static JTextField userText;
+    private static JLabel passLabel;
+    private static JTextField passInput;
+    private static JButton button;
+    private static JLabel success;
+
 
     public static void main(String[] args) {
         JPanel panel = new JPanel();
@@ -11,23 +21,41 @@ public class GUI {
 
         panel.setLayout(null);
 
-        JLabel userLabel = new JLabel("User");
+        userLabel = new JLabel("User");
         userLabel.setBounds(10, 20, 80, 25);
         panel.add(userLabel);
 
-        JTextField userText = new JTextField(20);
+        userText = new JTextField(20);
         userText.setBounds(100, 20, 165, 25);
         panel.add(userText);
 
-        JLabel passLabel = new JLabel("Password");
+        passLabel = new JLabel("Password");
         passLabel.setBounds(10, 50, 80, 25);
         panel.add(passLabel);
 
-        JTextField passInput = new JPasswordField(20);
+        passInput = new JPasswordField(20);
         passInput.setBounds(100, 50, 165, 25);
         panel.add(passInput);
+
+        button = new JButton("Submit");
+        button.setBounds(10, 80, 80, 25);
+        button.addActionListener(new GUI());
+        panel.add(button);
+
+        success = new JLabel("");
+        success.setBounds(10, 110, 300, 25);
+        panel.add(success);
 
         frame.setVisible(true);
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String user = userText.getText();
+        String password = passInput.getText();
+
+        if(user.equals("Jeff") & password.equals("1234")) {
+            success.setText("YOU DID IT");
+        }
+    }
 }
